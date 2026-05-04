60e535d269b7a31cb175cbdecf7ea28282191ec3
Naim3097 <sales@nexovadigital.com>
2026-05-04 13:48:38 +0800

    Make navbar logo 30% smaller on mobile (h-7 mobile, h-10 desktop)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
             alt="First Class Credit"
             width={160}
             height={50}
-            className={`h-10 w-auto object-contain transition-all duration-500 ${
+            className={`h-7 lg:h-10 w-auto object-contain transition-all duration-500 ${
               scrolled || open ? "" : "brightness-0 invert"
             }`}
             priority
